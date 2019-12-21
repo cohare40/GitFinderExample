da73dfdf222318125ca058d78e93b595e24bd374
Cameron O'Hare <cameronohare.98@hotmail.co.uk>
2019-12-21 16:15:51 +0000

    Added UserItem + Navbar components

---
+++ src/components/users/Users.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "id",
+        login: "mojumbo",
+        avatar_url: "https://avatars2.githubusercontent.com/u/1?s=460&v=4",
+        html_url: "https://github.com/mojombo"
+      },
+      {
+        id: "id",
+        login: "mojumbo",
+        avatar_url: "https://avatars2.githubusercontent.com/u/1?s=460&v=4",
+        html_url: "https://github.com/mojombo"
+      },
+      {
+        id: "id",
+        login: "mojumbo",
+        avatar_url: "https://avatars2.githubusercontent.com/u/1?s=460&v=4",
+        html_url: "https://github.com/mojombo"
+      }
+    ]
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Users;
